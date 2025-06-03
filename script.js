7b58d2944ad5b6b462260ea8e8e0a3b552c89279
Invitación <nicod.56b@gmail.com>
2025-06-03 16:41:28 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,18 +10,12 @@ const playBtn = document.getElementById("play-button");
 const audio = document.getElementById("background-music");
 const firstSlideImg = document.querySelector(".swiper-slide:first-child img");
 
-// Evento al hacer clic en el botón de música
+// Evento clic botón
 playBtn.addEventListener("click", () => {
-  // Reproducir música
   audio.play();
-
-  // Agregar animación de desvanecimiento al botón
   playBtn.classList.add("fade-out");
-
-  // Agregar clase para animación de empujón a la imagen
   firstSlideImg.classList.add("img-nudge");
 
-  // Ocultar el botón después de 1 segundo
   setTimeout(() => {
     playBtn.style.display = "none";
   }, 1000);
